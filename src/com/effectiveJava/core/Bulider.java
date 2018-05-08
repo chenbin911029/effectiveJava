c1bc1ed2271f49f1fe3f0d1c337c4761dde90a62
unknown <bin.chen@360jie.com.cn>
2018-05-08 10:07:33 +0800

    Initial commit

---
+++ src/com/effectiveJava/core/Bulider.java
@@ -0,0 +1,11 @@
+package com.effectiveJava.core;
+
+/**
+ * Description:
+ * User: chenbin-pc
+ * Date: 2018-05-08
+ * Time: 9:49
+ */
+public interface Bulider {
+    
+}
---
+++ src/com/effectiveJava/core/charcter02/Bulider.java
@@ -0,0 +1,11 @@
+package com.effectiveJava.core.charcter02;
+
+/**
+ * Description:A bulider for objects of type T
+ * User: chenbin-pc
+ * Date: 2018-05-08
+ * Time: 9:49
+ */
+public interface Bulider<T> {
+    public T bulider();
+}
